3364521eb986b746426b6667aa2297e2ee659f50
UghZan <yayminecrafters@gmail.com>
2021-02-28 19:50:31 +0300

    Добавьте файлы проекта.

---
+++ RPGDatabase.DomainModel/Item.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPGDatabase.DomainModel
+{
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        Consumable,
+        QuestItem,
+        Misc
+    }
+
+    public class Item
+    {
+        public string ID { get; set; }
+
+        public string Name { get; set; }
+
+        public ItemType Type { get; set; }
+
+        public int Price { get; set; }
+
+        public int Rarity { get; set; }
+
+        public Player Owner { get; set; }
+    }
+}
